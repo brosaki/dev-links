dad67f919994b6eddd39c08860e9a8264414f517
Manosaki1 <tyxytbr@hotmail.com>
2023-01-08 20:52:17 -0300

    set darkmode default

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     html.classList.remove("light")
-    img.setAttribute("style", "visibility: hidden")
   } else {
     html.classList.add("light")
-    img.setAttribute("style", "visibility: visible")
   }
 }
